14caeaa541b653e5479b282db9ab87ca421a2d02
azizbelkhouja <belkhoujamohamedaziz@gmail.com>
2024-04-08 18:38:01 +0100

    printBoard function

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+char Board[3][3];
+
+void printBoard();
+
+int main()
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        Board[i][j] = ' ';
+    }
+
+    printBoard();
+}
+
+void printBoard()
+{
+    printf(" %c | %c | %c\n", Board[0][0], Board[0][1], Board[0][2]);
+    printf("---|---|---\n");
+    printf(" %c | %c | %c\n", Board[1][0], Board[1][1], Board[1][2]);
+    printf("---|---|---\n");
+    printf(" %c | %c | %c\n", Board[2][0], Board[2][1], Board[2][2]);
+} 
