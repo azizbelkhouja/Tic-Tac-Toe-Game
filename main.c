eb1b1cae4230712ebc21ba461b968854b7e06570
azizbelkhouja <belkhoujamohamedaziz@gmail.com>
2024-04-09 15:47:30 +0100

    might be back later

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 char Board[3][3];
 const char PLAYER = 'X';
@@ -8,8 +9,9 @@ int emptyplaces = 9;
 
 void clearBoard();
 void printBoard();
+bool isempty(int row, int column);
 void playerMove();
-bool isempty();
+void computerMove();
 
 int main()
 {
@@ -18,9 +20,14 @@ int main()
 
     while (emptyplaces > 0)
     {
+        /*
+            ! when the player plays and the case is full, the computer still plays, then we go back to the player
+            TODO : don't allow the computer to play unless the player plays in an empty case
+        */
         printBoard();
         playerMove();
-        printBoard();
+        computerMove();
+
     }
 
 
@@ -49,12 +56,18 @@ void printBoard()
     printf(" %c | %c | %c\n", Board[2][0], Board[2][1], Board[2][2]);
 } 
 
+bool isempty(int row, int column)
+{
+    if (Board[row][column] == ' ') return true;
+    else return false;
+}
+
 void playerMove()
 {
     int playerRow, playerColumn;
 
     do {
-        
+
         printf("insert row (1-3): ");
         scanf("%d", &playerRow);
         printf("insert column (1-3): ");
@@ -67,12 +80,22 @@ void playerMove()
         } else printf("\ncase not empty, choose again\n");
 
     } while ((playerRow < 1 || playerRow > 3 || playerColumn < 1 || playerColumn > 3) && !isempty(playerRow-1, playerColumn-1));
-    
-    
+
 }
 
-bool isempty(int row, int column)
+void computerMove()
 {
-    if (Board[row][column] == ' ') return true;
-    else return false;
+    int randomrow;
+    int randomcolumn;
+
+    do {
+
+        randomrow = rand() % 3;
+        randomcolumn = rand() % 3;
+
+    } while(!isempty(randomrow, randomcolumn));
+
+    Board[randomrow][randomcolumn] = COMPUTER;
+    emptyplaces--;
 }
+
