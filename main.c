75396b816c075a89512bd8b495ac067f5005f9d6
azizbelkhouja <belkhoujamohamedaziz@gmail.com>
2024-04-09 17:35:30 +0100

    tic tac toe

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <time.h>
 
 char Board[3][3];
 const char PLAYER = 'X';
@@ -12,25 +13,38 @@ void printBoard();
 bool isempty(int row, int column);
 void playerMove();
 void computerMove();
+bool checkwinner(char winner);
 
 int main()
 {
 
-    clearBoard();
+    srand(time(NULL));
+
+    //printf("\ncheck winner = %s\n", checkwinner(PLAYER));
+
+    clearBoard(); 
 
     while (emptyplaces > 0)
     {
-        /*
-            ! when the player plays and the case is full, the computer still plays, then we go back to the player
-            TODO : don't allow the computer to play unless the player plays in an empty case
-        */
         printBoard();
         playerMove();
+        if (checkwinner(PLAYER)) {
+            printBoard();
+            printf("You win!\n");
+            break;
+        }
+        if (emptyplaces == 0) {
+            printBoard();
+            printf("a tie!\n");
+            break;
+        }
         computerMove();
-
+        if (checkwinner(COMPUTER)) {
+            printBoard();
+            printf("Computer wins!\n");
+            break;
+        }
     }
-
-
 
     return 0;
 }
@@ -58,8 +72,7 @@ void printBoard()
 
 bool isempty(int row, int column)
 {
-    if (Board[row][column] == ' ') return true;
-    else return false;
+    return Board[row][column] == ' ';
 }
 
 void playerMove()
@@ -67,19 +80,21 @@ void playerMove()
     int playerRow, playerColumn;
 
     do {
-
         printf("insert row (1-3): ");
         scanf("%d", &playerRow);
         printf("insert column (1-3): ");
         scanf("%d", &playerColumn);
 
-        if(isempty(playerRow-1, playerColumn-1))
-        {
-        Board[playerRow-1][playerColumn-1] = PLAYER;
-        emptyplaces --;
-        } else printf("\ncase not empty, choose again\n");
-
-    } while ((playerRow < 1 || playerRow > 3 || playerColumn < 1 || playerColumn > 3) && !isempty(playerRow-1, playerColumn-1));
+        if (playerRow < 1 || playerRow > 3 || playerColumn < 1 || playerColumn > 3) {
+            printf("Invalid input. Row and column must be between 1 and 3.\n");
+        } else if (!isempty(playerRow-1, playerColumn-1)) {
+            printf("Case not empty, choose again.\n");
+        } else {
+            Board[playerRow-1][playerColumn-1] = PLAYER;
+            emptyplaces--;
+            break;
+        }
+    } while (true);
 
 }
 
@@ -98,4 +113,34 @@ void computerMove()
     Board[randomrow][randomcolumn] = COMPUTER;
     emptyplaces--;
 }
+
+bool checkwinner(char winner)
+{
+    // rows
+    for (int i = 0; i < 3; i++) 
+    {
+        if (Board[i][0] == winner && Board[i][1] == winner && Board[i][2] == winner) 
+        {
+            return true;
+        }
+    }
+    
+    // columns
+    for (int j = 0; j < 3; j++) 
+    {
+        if (Board[0][j] == winner && Board[1][j] == winner && Board[2][j] == winner) 
+        {
+            return true;
+        }
+    }
+    
+    // diagonals
+    if ((Board[0][0] == winner && Board[1][1] == winner && Board[2][2] == winner) || (Board[0][2] == winner && Board[1][1] == winner && Board[2][0] == winner)) 
+    {
+        return true;
+    }
+    
+    return false;
+}
+
 
